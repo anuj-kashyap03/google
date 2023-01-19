2bfe4020b3425c1d43fcb94bb5dccc614225c6d3
anuj.kashyap.me@gmail.com <anujkashyp6575@gmail.com>
2023-01-19 23:35:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path= require('path')
+
+const port =process.env.PORT  || 5000
+
+const app = express()
+app.use(express.static(path.join(__dirname,'frontend/build')))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,'/frontend/build/index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`app is running on the port ${port}`)
+})
